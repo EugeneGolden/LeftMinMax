4d7279abeaf8d21e313e1dd678c9ca4115b9ed08
Eugene <yeug@gmail.com>
2015-12-03 23:59:12 +0200

    one more

--- MinMaxValue/Program.cs
+++ MinMaxValue/Program.cs
@@ -11,7 +11,7 @@ namespace MinMaxValue
     {
         //Дано натуральное число, в котором все цифры различны. Определить, какая цифра расположена в нем левее: максимальная или минимальная
 
-        public Array TransferIntToList(int value)
+        public Array TransferIntToArray(int value)
         {
             string row = Convert.ToString(value);          //the number is a string
             char[] array = row.ToCharArray();              //the string is an array
@@ -29,25 +29,44 @@ namespace MinMaxValue
             Array arrayLocal = arrayInput;
             int min = (int)arrayLocal.GetValue(0);
             int max = (int)arrayLocal.GetValue(0);
+            int indexMin = 0;
+            int indexMax = 0;
             
             for (int i = 1; i < (arrayLocal.Length - 1); i++)
             {
                 if ((int)arrayLocal.GetValue(i) < min)
                 {
                     min = (int)arrayLocal.GetValue(i);
+                    indexMin = i;
+                }
+                if ((int)arrayLocal.GetValue(i) > max)
+                {
+                    max = (int)arrayLocal.GetValue(i);
+                    indexMax = i;
                 }
             }
+            if (indexMax > indexMin)
+            {
+                Console.WriteLine("Минимальное значение " + min + " левее");
+            }
+            else
+            {
+                Console.WriteLine("Максимальное значение " + max + " левее");
+            }
+            return max;
         }
         static void Main(string[] args)
         {
             int n;
             ArrayList myList;
+            Array myArray;
+            int result;
+            
             Console.Write("Enter an integer number: ");
             n = int.Parse(Console.ReadLine());
             LeftMinMax lmm = new LeftMinMax();
-            myList = lmm.TransferIntToList(n);
-            var count = myList.Count;
-            Console.WriteLine(myList);
+            myArray = lmm.TransferIntToArray(n);
+            result = lmm.MinMax(myArray);
             Console.ReadKey();
 
 
